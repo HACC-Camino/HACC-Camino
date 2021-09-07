4eaddd2ba58b0fc2c4f5219dd72f602cd34102ec
micgainey <45333969+micgainey@users.noreply.github.com>
2021-09-07 00:14:21 -1000

    remade forms and created modal

---
+++ app/imports/ui/components/landing/SignInForm.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Form, Message, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+export default class SignInForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '', error: '' };
+  }
+
+  handleChange = (e, { name, value }) => {
+    this.setState({ [name]: value });
+  }
+
+  submit = () => {
+    const { email, password } = this.state;
+    Meteor.loginWithPassword(email, password, (err) => {
+      if (err) {
+        this.setState({ error: err.reason });
+      } else {
+        this.setState({ error: '', redirectToReferer: true });
+      }
+    });
+    console.log('1');
+  }
+
+  render() {
+    return (
+        <div>
+          <Segment>
+              <Form onSubmit={this.submit}>
+                <Segment stacked>
+                  <Form.Input
+                      label="Email"
+                      icon="user"
+                      iconPosition="left"
+                      name="email"
+                      type="email"
+                      placeholder="E-mail address"
+                      onChange={this.handleChange}
+                  />
+                  <Form.Input
+                      label="Password"
+                      icon="lock"
+                      iconPosition="left"
+                      name="password"
+                      placeholder="Password"
+                      type="password"
+                      onChange={this.handleChange}
+                  />
+                  <Form.Button content="Submit"/>
+                </Segment>
+              </Form>
+              <Message>
+                <Link to="/signup">Click here to Register</Link>
+              </Message>
+              {this.state.error === '' ? (
+                  ''
+              ) : (
+                  <Message
+                      error
+                      header="Login was not successful"
+                      content={this.state.error}
+                  />
+              )}
+          </Segment>
+        </div>
+    );
+  }
+}
