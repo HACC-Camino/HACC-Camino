14e4e92bd4ccd0ce03e670b355b4610f88504c38
Michael <mgainey@hawaii.edu>
2021-10-07 01:06:34 -1000

    not working

---
+++ app/imports/ui/components/Upload.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const { S3 } = require('@aws-sdk/client-s3');
+const aws = require('aws-sdk');
+
+const config = {
+    REACT_APP_ACCESS_ID: 'AKIAZ3L5B3UO6UJ4MT65',
+    REACT_APP_ACCESS_KEY: 'k6gRB9sfMZoYY14aJ8jstymxOScR2uC3iuGkXGWp',
+    REACT_APP_BUCKET_NAME: 'testbucketlumicamino',
+    REACT_APP_REGION: 'us-east-1',
+};
+
+// const s3 = new S3({
+//     region: config.REACT_APP_REGION,
+//     secretAcessKey: config.REACT_APP_ACCESS_KEY,
+//     accessKeyId: config.REACT_APP_ACCESS_ID,
+// });
+
+aws.config.update({
+    secretAcessKey: 'k6gRB9sfMZoYY14aJ8jstymxOScR2uC3iuGkXGWp',
+    accessKeyId: 'AKIAZ3L5B3UO6UJ4MT65',
+    region: config.REACT_APP_REGION,
+});
+
+const s3 = new aws.S3();
+
+class UploadCard extends React.Component {
+
+    uploadfun(e) {
+        console.log(e.target.files[0]);
+        console.log(e.target.name);
+        console.log(e.target.files[0].name);
+        s3.putObject({
+            Bucket: config.REACT_APP_BUCKET_NAME,
+            Key: e.target.files[0].name,
+            Body: e.target.files[0],
+            ACL: 'public-read',
+        }, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('sucess');
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>hello</h3>
+                <input type='file' onChange={this.uploadfun} />
+            </div>
+        );
+    }
+}
+
+export default UploadCard;
